a4c625af44b697090cf47b9cce60fe3de882fa70
vinaykatkuri50 <vinayreddy2610@gmail.com>
2025-05-09 23:35:40 +0530

    adding the Sum Odd & Sum Even Java files

--- src/test/java/basics/SumEven.java
+++ src/test/java/basics/SumEven.java
@@ -1,9 +1,27 @@
 package basics;
 
 public class SumEven {
+	
+	public void sumOfEven(int n[]) {
+		
+		int sum=0;
+		
+		for(int i=0;i<n.length;i++) {
+			if(n[i]%2==0) {
+				sum=sum+n[i];
+			}
+		}
+		System.out.println("Sum of Even numbers: "+sum);
+//		return sum;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		SumEven s=new SumEven();
+		s.sumOfEven(new int[] {1,4,3,5,6,8});
+		s.sumOfEven(new int[] {0,41,30,52,60,81,11});
 
 	}
 
--- src/test/java/basics/SumOdd.java
+++ src/test/java/basics/SumOdd.java
@@ -4,7 +4,15 @@ public class SumOdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int a[]= {12,13,5,7,9,4,6};
+		int sumOdd=0;
+		
+		for(int i=0;i<a.length;i++) {
+			if(a[i]%2!=0) {
+				sumOdd=sumOdd+a[i];
+			}
+		}
+		System.out.println(sumOdd);
 	}
-
 }
